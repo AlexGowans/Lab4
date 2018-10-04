2dde46c948031b2735bf29af3cd973ea4afeb29a
AlexGowans <hvnlymitsu@gmail.com>
2018-10-03 18:26:30 -0700

    Commented and Changed username settings to my real name hopefully maybe

--- Assets/Scripts/scrPlayerController.cs
+++ Assets/Scripts/scrPlayerController.cs
@@ -15,21 +15,21 @@ public class scrPlayerController : MonoBehaviour {
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        float moveJump = 0.0f;
+        float moveJump = 0.0f; //so you dont always get a jump
 
-        if(rb.velocity.y == 0 && Input.GetKeyDown(KeyCode.Space) ) {
+        if(rb.velocity.y == 0 && Input.GetKeyDown(KeyCode.Space) ) { // only jump if on ground
             moveJump = jumpPower;
         }
 
         Vector3 movement = new Vector3(moveHorizontal, moveJump, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * speed); // add the final total force
 
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Pick Up")) {
-            other.gameObject.SetActive(false);
+        if (other.gameObject.CompareTag("Pick Up")) { //makesure its the right object
+            other.gameObject.SetActive(false); 
         }
     }
 }
