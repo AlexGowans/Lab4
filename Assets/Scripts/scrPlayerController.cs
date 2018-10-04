2e5d9cf597e84f0c01d58c8b794bb7b4a0f885d3
HvnlyMitsu <hvnlymitsu@gmail.com>
2018-10-03 18:13:38 -0700

    Initial Commit

---
+++ Assets/Scripts/scrPlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrPlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpPower;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveJump = 0.0f;
+
+        if(rb.velocity.y == 0 && Input.GetKeyDown(KeyCode.Space) ) {
+            moveJump = jumpPower;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, moveJump, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
